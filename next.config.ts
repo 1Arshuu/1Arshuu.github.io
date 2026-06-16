c0137bb8a31afab2151ec7f33ce28d0ec98fb4ce
1Arshuu <arshuuhere@gmail.com>
2026-06-17 00:32:23 +0530

    Deploy to personal Github Page Site

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Arshu-Portfolio",
-  assetPrefix: "/Arshu-Portfolio/",
   trailingSlash: true,
 
   reactCompiler: true,
